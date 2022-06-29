2448d209b74abaf5a279b80d10a0813d9ca2e586
Richard <rcorbish@outlook.com>
2022-06-29 10:26:42 -0400

    Working - handle near singular matrix

--- rref.cpp
+++ rref.cpp
@@ -1,45 +1,59 @@
+#include <iostream>
+#include <string.h>
+#include <iomanip>
 
 #include "rref.hpp"
 
 extern "C" {
+//https://rosettacode.org/wiki/Reduced_row_echelon_form#C
 
     void rref( double *data, const size_t M, const size_t N ) {
-
-        double *p = data ;
 
         int lead = 0 ;
     
         for( int rix=0; rix<M; rix++ ) {
-            if( lead >= N )
-                return ;
-            int ix = rix;
-            p = data + lead * M ;            
-            while( *p == 0 ) {
-                ix++; 
-                p++ ;
-                if( ix == M ) {
-                    ix = rix;
-                    lead++;
-                    if( lead == N )
-                        return;
+            if( lead >= N ) return ;
+            int iix = rix ;
+
+            while( abs( data[iix + lead*M ] ) < 1e-11 ) {
+                iix++ ; 
+                if( iix == M ) {
+                    iix = rix ;
+                    lead++ ;
+                    if( lead == N ) return;
                 }
             }
 
             // MtxSwapRows(m, iix, rix );
+            if( iix != rix ) {
+                double *a = data + rix ;
+                double *b = data + iix ;
+                for( int i=0 ; i<N ; i++ ) {
+                    double tmp = *a ;
+                    *a = *b ;
+                    *b = tmp ;
+                    a += M ;
+                    b += M ;
+                }
+            }
 
             // MtxNormalizeRow(m, rix, lead );
-            p = data + rix + lead*M ;
+            double *p = data + rix + lead*M ;
             double lead_value = *p ;
-            for( int i=lead ; i<N ; i++ ) {
-                *p /= lead_value ;
-                p += M ;
+            if( abs(lead_value) > 1e-12 ) {
+                p = data + rix ;
+                for( int i=0 ; i<N ; i++ ) {
+                    *p /= lead_value ;
+                    p += M ;
+                }
             }
+            // matprint( data, M, N ) ;
 
             for( int iix=0; iix<M; iix++) {
                 if ( iix != rix ) {
                     p = data + iix + (lead * M) ;
                     // double lv = MtxGet(m, iix, lead );
-                    double lead_value = *p ;
+                    double lead_value = -(*p) ;
                     // MtxMulAndAddRows(m,iix, rix, -lv) ;
                     double *drow = data + iix ;
                     double *srow = data + rix ;
@@ -49,6 +63,7 @@ extern "C" {
                         srow += M ;
                     }
                 }
+                // matprint( data, M, N ) ;
             }
             lead++;
         }
@@ -66,4 +81,86 @@ extern "C" {
 // //	MtxDisplay(m);
 // }
 
+/**
+ * @ref https://adamdhalla.medium.com/linear-algebra-3-solving-ax-0-free-variables-and-pivot-variables-and-echelon-form-ce8cca411e60
+ * 
+ * @brief 
+ * 
+ * @param data 
+ * @param M 
+ * @param N 
+ */
+
+    int nullspace( double *data, const size_t M, const size_t N ) {
+        rref( data, M, N ) ;
+        bool pivots[N] ;
+
+        int numPivots = 0 ;        
+        int lead = 0 ;
+        double *p = data ;
+        for( int i=0 ; i<N && i<M; i++ ) {
+            if( abs(*p) > 1e-11 ) {
+                pivots[i] = true ;
+                numPivots++ ;
+                p += M+1 ; // skip down diagonal
+                lead++ ;
+            } else {
+                pivots[i] = false ;
+                while( abs(*p) < 1e-11 ) {
+                    p += M ;
+                    lead++ ;
+                    if( lead >= N ) goto escape ;
+                }
+            }
+        }
+        escape:
+        int numFreeVariables = N - numPivots ;
+        double nullspace[N*numFreeVariables] ;
+        memset( nullspace, 0, sizeof( nullspace) ) ;
+        p = nullspace ;
+        for( int i=0 ; i<N ; i++ ) {
+            if( !pivots[i] ) { 
+                *p = 1 ;
+                p += N ;
+            }
+            p++ ;
+        }
+
+        // matprint( nullspace, N, numFreeVariables ) ;
+
+        int pivot = 0 ;
+        p = data ;
+        for( int i=0 ; i<N ; i++ ) {  // down nullspace
+            if( pivots[i] ) { 
+                for( int f=0 ; f<numFreeVariables ; f++ ) { // across nullspace
+                    double total = 0 ;
+                    double *p2 = nullspace + f*N ;
+                    for( int j=0 ; j<N ; j++ ) {  // across rref (data)
+                        total += p[M*j] * -(*p2) ;
+                        p2++ ;
+                    }                
+                    nullspace[i+f*N] = total ;
+                }
+                pivot++ ;
+                p++ ;
+                if( pivot >= numPivots ) break ;
+            }
+            // matprint( nullspace, N, numFreeVariables ) ;
+        }
+        // matprint( nullspace, N, numFreeVariables ) ;
+        memcpy( data, nullspace, sizeof(nullspace) ) ;
+        return numFreeVariables ;
+    }
+    
+    void matprint( double *data, int M, int N ) {
+        double *p = data ;
+        for( int r=0 ; r<M ; r++ ) { 
+            for( int c=0 ; c<N ; c++ ) {
+                std::cout << std::setw(5) << p[c*M] << " " ;
+            }
+            std::cout << std::endl ;
+            p++ ;
+        }
+        std::cout << std::endl ;
+    }
 }
--- test.cpp
+++ test.cpp
@@ -1,35 +1,37 @@
 
-#include <iostream>
-#include "rref.hpp"
+#include <cstdlib>
 
-void matprint( double *data, int M, int N ) ;
+#include <iomanip>
+#include <iostream>
+#include <random>
+
+#include "rref.hpp"
 
 
 int main( int argc, char **argv, char **envp ) {
 
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(-100,100);
+
+	
 //  1    2   -1   -4
 //  2    3   -1   -11
 // -2    0   -3    22
 
     int M = 3 ;
-    int N = 4 ;
-    double data[] = { 1, 2, -2,   2, 3, 0,   -1, -1, -3,   -4, -11, 22 } ;
+    int N = 5 ;
+    double data[] = { -3,1,2,   6,-2,-4,  -1,2,5,  1,3,8,  -7,-1,-4 } ;
+    //double data[] = { 2,-3,1,1,4,1,7,-5,4,-7,-6,-5,2,3,2 } ;
+    // double *data = (double*)std::malloc( sizeof(double) * M * N ) ;
+    // for( int i=0 ; i<M*N ; i++ ) {
+    //     data[i] = distribution(generator) ;
+    // }
+
     matprint( data, M, N ) ;
-    rref( data, M, N ) ;
-    matprint( data, M, N ) ;
+    int freeVars = nullspace( data, M, N ) ;
+    matprint( data, N, freeVars ) ;
 
     return 0 ;
 }
 
 
-void matprint( double *data, int M, int N ) {
-    double *p = data ;
-    for( int r=0 ; r<M ; r++ ) { 
-        for( int c=0 ; c<N ; c++ ) {
-            std::cout << p[c*M] << " " ;
-        }
-        std::cout << std::endl ;
-        p++ ;
-    }
-    std::cout << std::endl ;
-}
